7cd30878db391eb2450bc306e2f5525dd6d67b43
aponamarev <alex.ponamaryov@gmail.com>
2018-06-23 19:37:00 +0000

    Created numpy version of the IK module. Fixed an error in tests.

--- IK_opt_debug.py
+++ IK_opt_debug.py
@@ -9,119 +9,139 @@ You can generate additional test cases by setting up your kuka project and runni
 From here you can adjust the joint angles to find thetas, use the gripper to extract positions and orientation (in quaternion xyzw) and lastly use link 5
 to find the position of the wrist center. These newly generated test cases can be added to the test_cases dictionary.
 '''
-def tf_matrix(alpha, a, d, q):
-    # Creates a transformation matrix based yaw, pitch, roll rotations
-    # for around x, y, z rotations
-    tf = Matrix([
-        [cos(q)             ,             -sin(q),              0,              a],
-        [sin(q)*cos(alpha)  ,   cos(q)*cos(alpha),    -sin(alpha),  -sin(alpha)*d],
-        [sin(q)*sin(alpha)  ,   cos(q)*sin(alpha),      cos(alpha),  cos(alpha)*d],
-        [0                  ,                   0,               0,             1]
-    ])
-    return tf
+class IK(object):
 
-def eval_WC(px, py, pz, rot_ee):
-    EE = Matrix([
-        [px],
-        [py],
-        [pz]
-    ])
-    # Calculate wrist center (wc)
-    WC = EE - 0.303 * rot_ee[:, 2]
-    return WC
+    q1, q2, q3, q4, q5, q6, q7 = None, None, None, None, None, None, None
+    r0_3 = None
+    t0_ee = None
+    t01 = None
+    t12 = None
+    t23 = None
+    __rot_ee = None
+    @property
+    def rot_ee(self):
+        if self.__rot_ee is None:
+            self.__rot_ee = self._rot_matrix()
+        return self.__rot_ee
 
-def rot_matrix():
-    r, p, y = symbols('r p y')
-    rot_x = Matrix([
-        [1, 0, 0],
-        [0, cos(r), -sin(r)],
-        [0, sin(r), cos(r)]
-    ])
-    rot_y = Matrix([
-        [cos(p), 0, sin(p)],
-        [0, 1, 0],
-        [-sin(p), 0, cos(p)]
-    ])
 
-    rot_z = Matrix([
-        [cos(y), -sin(y), 0],
-        [sin(y),  cos(y), 0],
-        [0, 0, 1]
-    ])
+    def __init__(self):
+        # Create DH parameter symbols
+        # d parameter is a link offset along z axis (between x0 and x1 axis)
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        # a parameter is a link length along x axis (distance between z0 and z1)
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        # alpha parameter is a twist angle (rotation angle of z axis along x axis) according to
+        # right hand rule (counter-clock wise)
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+        # q (theta) parameter is joint angle (rotation angle of x axis along z axis) according to
+        # the right hand rule (counter-clock wise)
+        self.q1, self.q2, self.q3, self.q4, self.q5, self.q6, self.q7 = symbols('q1:8')
+        # Create a dictionary containing df table
+        dh_table = {
+            alpha0: 0.0, a0: 0.00, d1: 0.75, self.q1: self.q1,
+            alpha1: -pi / 2.0, a1: 0.35, d2: 0.00, self.q2: -pi / 2.0 + self.q2,
+            alpha2: 0.0, a2: 1.25, d3: 0.00, self.q3: self.q3,
+            alpha3: -pi / 2.0, a3: -0.054, d4: 1.50, self.q4: self.q4,
+            alpha4: -pi / 2.0, a4: 0.00, d5: 0.00, self.q5: self.q5,
+            alpha5: -pi / 2.0, a5: 0.00, d6: 0.00, self.q6: self.q6,
+            alpha6: 0.0, a6: 0.00, d7: 0.303, self.q7: 0.0
+        }
 
-    rot_ee = rot_z * rot_y * rot_x
+        # Create transformation matrices
+        self.t01 = self._tf_matrix(alpha0, a0, d1, self.q1).subs(dh_table)
+        self.t12 = self._tf_matrix(alpha1, a1, d2, self.q2).subs(dh_table)
+        self.t23 = self._tf_matrix(alpha2, a2, d3, self.q3).subs(dh_table)
+        t34 = self._tf_matrix(alpha3, a3, d4, self.q4).subs(dh_table)
+        t45 = self._tf_matrix(alpha4, a4, d5, self.q5).subs(dh_table)
+        t56 = self._tf_matrix(alpha5, a5, d6, self.q6).subs(dh_table)
+        t6_ee = self._tf_matrix(alpha6, a6, d7, self.q7).subs(dh_table)
 
-    rot_z_adj = rot_z.subs(y, radians(180))
-    rot_y_adj = rot_y.subs(p, radians(-90))
+        self.t0_ee = self.t01 * self.t12 * self.t23 * t34 * t45 * t56 * t6_ee
 
-    rot_error = rot_z_adj * rot_y_adj
+        self.r0_3 = self.t01[0:3, 0:3] * self.t12[0:3, 0:3] * self.t23[0:3, 0:3]
 
-    rot_ee = rot_ee * rot_error
+    def _tf_matrix(self, alpha, a, d, q):
+        # Creates a transformation matrix based yaw, pitch, roll rotations
+        # for around x, y, z rotations
+        tf = Matrix([
+            [cos(q)             ,             -sin(q),              0,              a],
+            [sin(q)*cos(alpha)  ,   cos(q)*cos(alpha),    -sin(alpha),  -sin(alpha)*d],
+            [sin(q)*sin(alpha)  ,   cos(q)*sin(alpha),      cos(alpha),  cos(alpha)*d],
+            [0                  ,                   0,               0,             1]
+        ])
+        return tf
 
-    return rot_ee
+    def _eval_WC(self, px, py, pz, rot_ee):
+        EE = Matrix([
+            [px],
+            [py],
+            [pz]
+        ])
+        # Calculate wrist center (wc)
+        WC = EE - 0.303 * rot_ee[:, 2]
+        return WC
 
-def IK_thetas(px, py, pz, roll, pitch, yaw):
-    # Create DH parameter symbols
-    # d parameter is a link offset along z axis (between x0 and x1 axis)
-    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-    # a parameter is a link length along x axis (distance between z0 and z1)
-    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-    # alpha parameter is a twist angle (rotation angle of z axis along x axis) according to
-    # right hand rule (counter-clock wise)
-    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
-    # q (theta) parameter is joint angle (rotation angle of x axis along z axis) according to
-    # the right hand rule (counter-clock wise)
-    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-    # Create a dictionary containing df table
-    dh_table = {
-        alpha0:         0.0, a0:  0.00, d1: 0.75, q1: q1,
-        alpha1:   -pi / 2.0, a1:  0.35, d2: 0.00, q2: -pi / 2.0 + q2,
-        alpha2:         0.0, a2:  1.25, d3: 0.00, q3: q3,
-        alpha3:   -pi / 2.0, a3:-0.054, d4: 1.50, q4: q4,
-        alpha4:   -pi / 2.0, a4:  0.00, d5: 0.00, q5: q5,
-        alpha5:   -pi / 2.0, a5:  0.00, d6: 0.00, q6: q6,
-        alpha6:         0.0, a6:  0.00, d7:0.303, q7: 0.0
-    }
+    def _rot_matrix(self):
+        r, p, y = symbols('r p y')
+        rot_x = Matrix([
+            [1, 0, 0],
+            [0, cos(r), -sin(r)],
+            [0, sin(r), cos(r)]
+        ])
+        rot_y = Matrix([
+            [cos(p), 0, sin(p)],
+            [0, 1, 0],
+            [-sin(p), 0, cos(p)]
+        ])
 
-    # Create transformation matrices
-    t01     = tf_matrix(alpha0, a0, d1, q1).subs(dh_table)
-    t12     = tf_matrix(alpha1, a1, d2, q2).subs(dh_table)
-    t23     = tf_matrix(alpha2, a2, d3, q3).subs(dh_table)
-    t34     = tf_matrix(alpha3, a3, d4, q4).subs(dh_table)
-    t45     = tf_matrix(alpha4, a4, d5, q5).subs(dh_table)
-    t56     = tf_matrix(alpha5, a5, d6, q6).subs(dh_table)
-    t6_ee   = tf_matrix(alpha6, a6, d7, q7).subs(dh_table)
+        rot_z = Matrix([
+            [cos(y), -sin(y), 0],
+            [sin(y),  cos(y), 0],
+            [0, 0, 1]
+        ])
 
-    t0_ee   = t01 * t12 * t23 * t34 * t45 * t56 * t6_ee
+        rot_ee = rot_z * rot_y * rot_x
 
-    # create end-effector matrix
-    rot_ee = rot_matrix()
-    rot_ee = rot_ee.subs({'r': roll, 'p': pitch, 'y': yaw})
-    WC = eval_WC(px, py, pz, rot_ee)
+        rot_z_adj = rot_z.subs(y, radians(180))
+        rot_y_adj = rot_y.subs(p, radians(-90))
 
-    # Calculate joint angles using inverse kinematics
-    theta1 = atan2(WC[1], WC[0])
-    # SSS traingle for theta2 and theta3
-    s_a = 1.501
-    s_b = sqrt(pow(sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35, 2)
-               + pow(WC[2] - 0.75, 2))
-    s_c = 1.25
+        rot_error = rot_z_adj * rot_y_adj
 
-    aa = acos( (s_b * s_b + s_c * s_c - s_a * s_a) / (2 * s_b * s_c) )
-    ab = acos((s_a * s_a + s_c * s_c - s_b * s_b) / (2 * s_a * s_c))
-    #ac = acos((s_a * s_a + s_b * s_b - s_c * s_c) / (2 * s_a * s_b))
+        rot_ee = rot_ee * rot_error
 
-    theta2 = pi / 2 - aa - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
-    theta3 = pi / 2 - (ab + 0.036)
+        return rot_ee
 
-    r0_3 = t01[0:3, 0:3] * t12[0:3, 0:3] * t23[0:3, 0:3]
-    r0_3 = r0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
-    r3_6 = r0_3.inv("LU") * rot_ee
-    theta4 = atan2(r3_6[2, 2], -r3_6[0, 2])
-    theta5 = atan2(sqrt(r3_6[0, 2] * r3_6[0, 2] + r3_6[2, 2] * r3_6[2, 2]), r3_6[1, 2])
-    theta6 = atan2(-r3_6[1, 1], r3_6[1, 0])
 
-    return (theta1, theta2, theta3, theta4, theta5, theta6), t0_ee, WC
+    def eval(self, px, py, pz, roll, pitch, yaw):
+        # create end-effector matrix
+        rot_ee = self.rot_ee.subs({'r': roll, 'p': pitch, 'y': yaw})
+        WC = self._eval_WC(px, py, pz, rot_ee)
+
+        # Calculate joint angles using inverse kinematics
+        theta1 = atan2(WC[1], WC[0])
+        # SSS traingle for theta2 and theta3
+        s_a = 1.501
+        s_b = sqrt(
+            pow(sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35, 2)
+            + pow(WC[2] - 0.75, 2)
+        )
+        s_c = 1.25
+
+        aa = acos( (s_b * s_b + s_c * s_c - s_a * s_a) / (2 * s_b * s_c) )
+        ab = acos((s_a * s_a + s_c * s_c - s_b * s_b) / (2 * s_a * s_c))
+        #ac = acos((s_a * s_a + s_b * s_b - s_c * s_c) / (2 * s_a * s_b))
+
+        theta2 = pi / 2 - aa - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+        theta3 = pi / 2 - (ab + 0.036)
+
+        r0_3 = self.r0_3.evalf(subs={self.q1: theta1, self.q2: theta2, self.q3: theta3})
+        r3_6 = r0_3.inv("LU") * rot_ee
+        theta4 = atan2(r3_6[2, 2], -r3_6[0, 2])
+        theta5 = atan2(sqrt(r3_6[0, 2] * r3_6[0, 2] + r3_6[2, 2] * r3_6[2, 2]), r3_6[1, 2])
+        theta6 = atan2(-r3_6[1, 1], r3_6[1, 0])
+
+        return (theta1, theta2, theta3, theta4, theta5, theta6), self.t0_ee, WC
 
 test_cases = {1:[[[2.16135,-1.42635,1.55109],
                   [0.708611,0.186356,-0.157931,0.661967]],
@@ -172,9 +192,10 @@ def test_code(test_case):
     position = Position(test_case[0][0])
     orientation = Orientation(test_case[0][1])
 
-    comb = Combine(position,orientation)
-
+    comb = Combine(position, orientation)
     req = Pose(comb)
+
+    ik = IK()
     start_time = time()
     
     ########################################################################################
@@ -187,7 +208,7 @@ def test_code(test_case):
     roll, pitch, yaw = tf.transformations.euler_from_quaternion([x, y, z, w])
     ########################################################################################
     ## Insert IK code here!
-    thetas, t0_ee, WC = IK_thetas(px, py, pz, roll, pitch, yaw)
+    thetas, t0_ee, WC = ik.eval(px, py, pz, roll, pitch, yaw)
     theta1, theta2, theta3, theta4, theta5, theta6 = thetas
     ########################################################################################
     ## For additional debugging add your forward kinematics here. Use your previously calculated thetas
@@ -201,7 +222,7 @@ def test_code(test_case):
 
     ## For error analysis please set the following variables of your WC location and EE location in the format of [x,y,z]
     your_wc = [WC[0], WC[1], WC[2]] # <--- Load your calculated WC values in this array
-    your_ee = [fk[0,3], fk[1,3], fk[2,3]] # <--- Load your calculated end effector value from your forward kinematics
+    your_ee = [fk[0, 3], fk[1, 3], fk[2, 3]] # <--- Load your calculated end effector value from your forward kinematics
     ########################################################################################
 
     ## Error analysis
